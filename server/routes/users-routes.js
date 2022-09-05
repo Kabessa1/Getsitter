c54278e69860dc719e3bd2f34cb5ac2da12aef1e
Kabessa1 <noa9530134@gmail.com>
2022-09-05 10:49:44 +0300

    server changes1

--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const dbQuery = require('../DB/DB-query')
-const jwtauth = require('../jwtauth');
+const dbQuery = require("../DB/DB-query");
+const jwtauth = require("../jwtauth");
 
 // Get all users
 router.get("/users-profile", async (req, res) => {
@@ -9,11 +9,11 @@ router.get("/users-profile", async (req, res) => {
     const usersProfile = await dbQuery.getAllUsersProfile();
 
     if (usersProfile.length === 0) {
-    res.send("No users found");
+      res.send("No users found");
     } else if (usersProfile !== null && usersProfile !== undefined) {
-    res.send(usersProfile);
+      res.send(usersProfile);
     } else {
-    res.send("Some error on the server or DB");
+      res.send("Some error on the server or DB");
     }
   });
 });
@@ -33,16 +33,18 @@ router.get("/user-profile/:id", async (req, res) => {
   });
 });
 
-
 // update user by id
 router.put("/user-profile/:id", async (req, res) => {
   jwtauth.verifyToken(req, res, async (req, res) => {
-   try {
-    const userProfile = await dbQuery.UpdateUserProfileById(req.params.id, req.body);
-    res.send("User updated!");
-   } catch (error) {
-    res.send("Some error on the server or DB");
-   }
+    try {
+      const userProfile = await dbQuery.UpdateUserProfileById(
+        req.params.id,
+        req.body
+      );
+      res.send("{User updated!}");
+    } catch (error) {
+      res.send("Some error on the server or DB");
+    }
   });
 });
 
